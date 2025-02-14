45d86c79020aa26a404ac24a37a7d4d76d74d9e4
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-15 02:22:25 +0700

    Feat: Make Model Update User

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,6 +5,16 @@ async function readUser() {
     return await prisma.user.findMany();
 }
 
+async function updateUser(id, data) {
+    return await prisma.user.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: data
+    });
+}
+
 module.exports = {
-    readUser
+    readUser,
+    updateUser
 }
