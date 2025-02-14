8c7c64763f55df739cb873b819d244d3d9ff7caf
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-15 02:21:20 +0700

    Feat: Add Update User Route

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const UserController = require('../controller/UserController');
 // const accessValidation = require('../middleware/accessValidation');
 
 router.get('/', UserController.readUser)
+router.get('/{id}', UserController.updateUser)
 
 module.exports = router;
