23bf4da80964ba788c3232360e28c3483423a3fc
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-16 20:24:41 +0700

    Feat: Make Route Read Seller

--- src/routes/SellerRoutes.js
+++ src/routes/SellerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const SellerController = require('../controller/SellerController')
 
+router.get('/', SellerController.read)
 router.post('/', SellerController.create)
 
 module.exports = router
