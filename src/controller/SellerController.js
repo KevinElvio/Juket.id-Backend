e53dc0835291f6896b3e03eda21ea9668f7779c8
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-16 20:27:12 +0700

    Feat: Make Read Seller API

--- src/controller/SellerController.js
+++ src/controller/SellerController.js
@@ -18,6 +18,30 @@ const create = async (req, res) => {
     }
 }
 
+const read = async (req, res) => {
+    try {
+        const sellers = await sellerModel.readSeller();
+        if (sellers.length === 0) {
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Seller not found'
+            });
+        }
+        res.status(200).json({
+            status: 'success',
+            message: 'Seller retrieved successfully',
+            data: sellers
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            message: "Bad request",
+            serverMessage: error.message
+        });
+    }
+}
+
 module.exports = {
-    create
+    create,
+    read
 }
