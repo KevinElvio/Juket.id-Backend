50b97beb532c6ab326db79810ab72f095e98477d
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-18 14:55:19 +0700

    Feat: Make Create Category API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const UserRoutes = require('./src/routes/UserRoutes')
 const AuthRoutes = require('./src/routes/AuthRoutes')
 const SellerRoutes = require('./src/routes/SellerRoutes')
+const CategoryRoutes = require('./src/routes/CategoryRoutes')
 const app = express()
 const PORT = process.env.PORT || 5000
 const cors = require("cors");
@@ -13,8 +14,8 @@ app.use(cors());
 app.use('/user', UserRoutes)
 app.use('/auth', AuthRoutes)
 app.use('/seller', SellerRoutes)
+app.use('/category', CategoryRoutes)
 // app.use('/product', ProductRoutes)
-// app.use('/category', CategoryRoutes)
 
 
 app.listen(PORT, () => {
