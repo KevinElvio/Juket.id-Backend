e5a6ad89115991765e88b267abc2bae27e8e17e5
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-19 22:51:31 +0700

    Feat: Make Update Product Model

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -5,5 +5,6 @@ const ProductController = require('../controller/ProductController')
 router.post("/", ProductController.create)
 router.get("/", ProductController.read)
 router.get("/:id", ProductController.readById)
+router.put("/:id", ProductController.update)
 
 module.exports = router
