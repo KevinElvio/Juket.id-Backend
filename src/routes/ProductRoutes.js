889b8b9a315d2df9c0d8cf06c380ca5abbc9f6fd
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-19 17:00:01 +0700

    Feat: Make Read By Id Product Model

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -4,5 +4,6 @@ const ProductController = require('../controller/ProductController')
 
 router.post("/", ProductController.create)
 router.get("/", ProductController.read)
+router.get("/:id", ProductController.readById)
 
 module.exports = router
