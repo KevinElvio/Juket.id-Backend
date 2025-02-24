85da4430419528cb4a38e5fafad38d3164856f17
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-02-25 05:36:10 +0700

    Fix: add Multer for image

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const multer = require('multer')
+const path = require('path')
 const UserRoutes = require('./src/routes/UserRoutes')
 const AuthRoutes = require('./src/routes/AuthRoutes')
 const SellerRoutes = require('./src/routes/SellerRoutes')
@@ -10,13 +12,24 @@ const PORT = process.env.PORT || 5000
 const cors = require("cors");
 
 app.use(express.json())
-
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "/uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`)
+
+  }
+})
+const upload = multer({ storage })
+
 app.use('/user', UserRoutes)
 app.use('/auth', AuthRoutes)
 app.use('/seller', SellerRoutes)
 app.use('/category', CategoryRoutes)
-app.use('/product', ProductRoutes)
+app.use('/product',upload.single('image'), ProductRoutes)
 
 
 app.listen(PORT, () => {
